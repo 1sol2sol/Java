42dfafb9b799fe3c09bda81cab4897b4269ae8d7
HANSOL <HANSOL@DESKTOP-9NSNCV3>
2020-12-24 15:17:37 +0900

    day02 업로드

--- day02/src/Hello.java
+++
@@ -1,7 +0,0 @@
-import java.lang.*; // import > 준비명령
-
-public class Hello {
-	public static void main(String[] args) {
-      System.out.println("Hello everyone :)"); // ;> 출력명령 
-    }  // String, System > 누군가가 만들어놓은 도구(라이브러리)
-}
