94b22ebdd590b75214d0a91a4c1fb16debbb4380
HANSOL <HANSOL@DESKTOP-9NSNCV3>
2020-12-28 11:46:50 +0900

    입출력 테스트

---
+++ day05/src/output/Test02.java
@@ -0,0 +1,14 @@
+package output;
+import java.lang.*;
+public class Test02 {
+	public static void main(String[] args) {
+		System.out.println("-----------------------------------");
+		System.out.println("이름\t직금\t부서\t급여");
+		System.out.println("홍길동\t사장\t\t15000000");
+		System.out.println("이소룡\t부장\t경영지원\t5000000");
+		System.out.println("조자룡\t사원\t미래전략\t2500000");
+		System.out.println("-----------------------------------");
+		System.out.println("계\t\t\t22500000");
+		System.out.println("-----------------------------------");
+	}
+}
