ffbcef43dc4911e2207ad9ab343e9ae976583831
HANSOL <HANSOL@DESKTOP-9NSNCV3>
2020-12-29 18:21:05 +0900

    지하철 요금 계산 문제

---
+++ day06/src/condition/Test07_1.java
@@ -0,0 +1,41 @@
+package condition;
+import java.lang.*;
+import java.util.Calendar;
+import java.util.Scanner;
+public class Test07_1 {
+	public static void main(String[] args) {
+//<문제>
+//사용자에게 출생년도 4자리를 입력받아 나이를 계산하고 지하철 요금을 계산하세요
+//
+//지하철 요금표
+//- 어른신(65세이상) : 0원
+//- 성인(20세이상 65세미만) : 1250원
+//- 청소년(14세 이상 20세미만) : 720원
+//- 어린이(8세이상 14세미만) : 450원
+//- 유아(8세미만) : 0원
+		
+	//준비
+		Scanner sc = new Scanner(System.in);
+		System.out.println("출생년도 4자리를 입력하세요");
+		int birth = sc.nextInt();
+		sc.close();
+	//계산
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+		int age = year-birth+1;
+		System.out.println("age = " + age);
+				
+	//출력
+		if(age >= 65 || age < 8) {//무료 승차대상 = 어르신 + 유아
+			System.out.println("지하철 요금:0원");
+		}
+		else if(age >= 20) {
+			System.out.println("지하철 요금:1250원");
+		}
+		else if(age >= 14) {
+			System.out.println("지하철 요금:720원");
+		}
+		else {
+			System.out.println("지하철 요금 : 450원");
+		}
+	}
+}
