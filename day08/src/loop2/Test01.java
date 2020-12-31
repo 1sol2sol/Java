f2807db1e051e54cc6ca08198a6b42df48286ddf
HANSOL <HANSOL@DESKTOP-9NSNCV3>
2020-12-31 10:14:26 +0900

    for와 while 비교 예시

--- day08/src/loop2/Test01.java
+++ day08/src/loop2/Test01.java
@@ -1,10 +1,20 @@
 package loop2;
-
+//import java.lang.*;
 public class Test01 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+//	for를 이용하여 1부터 10까지 출력
+		for(int i=1; i<=10; i++) {
+			System.out.println("for=" + i);
+		}
+		
+//	while 반복
+		int n =1;
+		while(n<=10) {
+			System.out.println("while=" + n);
+			n++;
+		}
+		
 	}
 
 }
