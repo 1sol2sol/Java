fbf0fd36fe8b99ff52b09621707500541e30a622
HANSOL <HANSOL@HANSOL>
2021-01-10 23:16:01 +0900

    생성자 문제 풀이

--- day12/src/oop/constructor2/Test01.java
+++ day12/src/oop/constructor2/Test01.java
@@ -4,7 +4,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		
 		User a = new User("테스터1", "마법사");
-		User b = new User("테스터2", "전사", 5, 200);
+		User b = new User("테스터2", "전사", -1, -200);
 		User c = new User("운영자", "마법사", 999, 999999);
 		
 		a.output();
