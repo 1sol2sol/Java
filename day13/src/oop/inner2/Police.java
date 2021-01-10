0316de16c304d9ce6c2763f71ec28ff95a63106d
HANSOL <HANSOL@HANSOL>
2021-01-10 23:12:44 +0900

    중첩클래스 예제

---
+++ day13/src/oop/inner2/Police.java
@@ -0,0 +1,12 @@
+package oop.inner2;
+//경찰
+public class Police {
+	//멤버변수
+	 Gun gun;
+	 
+	 
+	 //Police에 종속된 클래스 = 중첩 클래스 
+	 public class Gun {
+
+	 }
+}
---
+++ day13/src/oop/inner2/Test01.java
@@ -0,0 +1,12 @@
+package oop.inner2;
+
+public class Test01 {
+	public static void main(String[] args) {
+		Police p = new Police();
+		//p.gun = new Gun();
+		
+		Theif t = new Theif();
+		//t.gun = new Gun();
+	}
+	
+}
